8381c0d411d0e2a00ddd7f5932c40106bc5dd154
Carmine Watcher <CWatcher@student.21-school.r>
2022-04-17 12:33:13 +0300

    [boilerplate] +ex01

--- ex00/Program.java
+++ ex00/Program.java
@@ -1,3 +1,4 @@
+package ex00;
 class Program {
 
 	public static void main(String[] args) {
---
+++ ex01/Program.java
@@ -0,0 +1,11 @@
+package ex01;
+
+import java.util.Scanner;
+
+class Program {
+
+	public static void main( String[] args ) {
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(n);
+	}
+}
