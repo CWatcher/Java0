5c4992f6f2940bb546baad996062a1afe9530a10
Carmine Watcher <CWatcher@student.21-school.r>
2022-04-17 14:37:10 +0300

    [rf] -else

--- ex01/Program.java
+++ ex01/Program.java
@@ -14,11 +14,14 @@ class Program {
 			System.out.println( "IllegalArgument" );
 			System.exit( -1 );
 		}
-		int i = 2;
-		for ( ; n % i != 0 && i * i <= n; i++ ) {};
-		if ( i * i > n )
-			System.out.println( true + " " + (i - 1) );
-		else
-			System.out.println( false + " " + (i - 1) );
+
+		boolean prime = true;
+		int i;
+		for ( i = 2; prime && i * i <= n; i++ ) {
+			if ( n % i == 0 ) {
+				prime = false;
+			}
+		};
+		System.out.println( prime + " " + (i - 1) );
 	}
 }
