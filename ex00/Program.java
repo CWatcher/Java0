11b4cebf50f48bdec16e1079a9e51ebe90411bb7
Carmine Watcher <CWatcher@student.21-school.r>
2022-04-17 15:05:46 +0300

    -condition and loop operators

--- ex00/Program.java
+++ ex00/Program.java
@@ -1,16 +1,16 @@
 class Program {
+
 	public static void main(String[] args) {
 		int n = -123456;
-		if (n < 0)
-			n = - n;
-		int divisor = 100000;
-		int	sum = 0;
-		while (n > 0) {
-			int digit = n / divisor;
-			sum += digit;
-			n -= divisor * digit;
-			divisor /= 10;
-		}
-		System.out.println( sum );
+		n = (n + (n >> 31)) ^ (n >> 31);
+
+		int	r = n % 10;
+		r += (n /= 10 ) % 10;
+		r += (n /= 10 ) % 10;
+		r += (n /= 10 ) % 10;
+		r += (n /= 10 ) % 10;
+		r += (n / 10 ) % 10;
+
+		System.out.println( r );
 	}
 }
