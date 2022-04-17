b6e22be664e036ab9bf79eef771b5364bddcc14e
Carmine Watcher <CWatcher@student.21-school.r>
2022-04-17 13:08:50 +0300

    [done] ex01

--- ex01/Program.java
+++ ex01/Program.java
@@ -6,6 +6,16 @@ class Program {
 
 	public static void main( String[] args ) {
 		int n = new Scanner(System.in).nextInt();
-		System.out.println(n);
+
+		if ( n < 2 ) {
+			System.out.println( "IllegalArgument" );
+			System.exit( -1 );
+		}
+		int i = 2;
+		for ( ; n % i != 0 && i * i <= n; i++ ) {};
+		if ( i * i > n )
+			System.out.println( true + " " + (i - 1) );
+		else
+			System.out.println( false + " " + (i - 1) );
 	}
 }
