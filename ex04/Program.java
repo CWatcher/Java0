c573ac807ef225eaef8bd3e4da0383b2eede5080
Carmine Watcher <cwatcher@student.21-school.r>
2022-09-30 20:56:01 +0300

    ex04 += charFrequencies

--- ex04/Program.java
+++ ex04/Program.java
@@ -3,11 +3,30 @@ package ex04;
 import java.util.Scanner;
 
 public class Program {
+	public static final short ChartSize = 10;
+	static class CharFrequency {
+		public char ch;
+		public short fr;
+	}
 	public static void main(String[] args) {
 		String s = new String(new Scanner(System.in).next());
 		short[] frequencies = new short[(int)Character.MAX_VALUE];
 
 		for (char ch: s.toCharArray())
 			frequencies[(int)ch]++;
+
+		CharFrequency[] charFrequencies = new CharFrequency[ChartSize];
+		for (int i = 0; i < charFrequencies.length; i++)
+			charFrequencies[i] = popMaxFrequency(frequencies);
+	}
+	static CharFrequency popMaxFrequency(short[] frequencies) {
+		CharFrequency charFrequency = new CharFrequency();
+		for (int i = 0; i < frequencies.length; i++)
+			if (frequencies[i] > charFrequency.fr) {
+				charFrequency.ch = (char)i;
+				charFrequency.fr = frequencies[i];
+			}
+		frequencies[(int)charFrequency.ch] = 0;
+		return charFrequency;
 	}
 }
